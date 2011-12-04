b1f3da2fb40d83b779387283bf325b8bc02d5b52
Corey Leigh Latislaw <colabug@gmail.com>
2011-12-03 20:31:03 -0500

    Added empty list check, used in addFront(); updated comments

--- Java/LinkedList/LinkedList.java
+++ Java/LinkedList/LinkedList.java
@@ -11,16 +11,28 @@ public class LinkedList {
 
     public void addFront(int nodeData)
     {
-        if (head == null) 
+        // Empty list, made node head
+        if (isEmpty())
         {
             Node node = new Node(nodeData, null);
             head = node;
         }
+        // Populated list
         else
         {
             Node node = new Node(nodeData, head.next);
             head = node;
         }
+    }
+
+    public boolean isEmpty()
+    {
+        if (head == null)
+        {
+            return true;
+        }
+
+        return false;
     }
 
     public void printList()
