ab6cd015566861689d3bdc2533303ece792b9b2a
Corey Leigh Latislaw <colabug@gmail.com>
2011-12-11 11:47:27 -0500

    Testing add 2 elements to front of list

--- Java/LinkedList/test/LinkedListTest.java
+++ Java/LinkedList/test/LinkedListTest.java
@@ -2,12 +2,14 @@ import com.colabug.LinkedList;
 import org.junit.Before;
 import org.junit.Test;
 
+import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
 
 public class LinkedListTest
 {
     private LinkedList ll;
     private static int nodeData  = 4;
+    private static int nodeData2 = 17;
 
     @Before
     public void setUp()
@@ -34,6 +36,17 @@ public class LinkedListTest
 
         // Confirm element is at front of list
         assertEquals( nodeData, ll.getHead() );
+    }
+
+    @Test
+    public void testAdd2ElementsToFront()
+    {
+        // Add elements
+        ll.addFront( nodeData );
+        ll.addFront( nodeData2 );
+
+        // Confirm second added element is at the front
+        assertEquals( nodeData2, ll.getHead() );
     }
 
 }
