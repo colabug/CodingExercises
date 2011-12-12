6aa068360834531dc7d1465a5f85874010ad0fa3
Corey Leigh Latislaw <colabug@gmail.com>
2011-12-12 18:04:35 -0500

    * Added test for empty queue * Added code for determining an empty queue * Added LinkedList as underlying data structure

--- Java/Queue/src/com/colabug/queue/Queue.java
+++ Java/Queue/src/com/colabug/queue/Queue.java
@@ -1,5 +1,13 @@
 package com.colabug.queue;
 
+import java.util.LinkedList;
+
 public class Queue<T>
 {
+    private final LinkedList<T> linkedList = new LinkedList<T>();
+
+    public boolean isEmpty()
+    {
+        return linkedList.isEmpty();
+    }
 }
--- Java/Queue/test/com/colabug/queue/QueueTest.java
+++ Java/Queue/test/com/colabug/queue/QueueTest.java
@@ -2,11 +2,14 @@ package com.colabug.queue;
 
 import org.junit.Test;
 
+import static junit.framework.Assert.assertTrue;
+
 public class QueueTest
 {
     @Test
-    public void testSomething()
+    public void testEmptyQueue()
     {
-
+        Queue<Integer> integerQueue = new Queue<Integer>();
+        assertTrue( integerQueue.isEmpty() );
     }
 }
